b48d14007d16577c566dc9673d2605a779ec0000
kai <tkkaifo@gmail.com>
2017-11-13 00:57:28 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.ssss

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 // List the full names of ALL group members at the top of your code.
-// Group Members: Gongtao Yang,
+// Group Members: Gongtao Yang, Kai Qian, Conley Gao
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -88,6 +88,26 @@ char *progScanner(FILE *ipf, char * istbuff ){
         printf("String input is %s \n", istbuff);
 /**********************InputProcess************************/
 //check matching "()"
+int my_strcmp (const char * s1, const char * s2)
+{
+    for(; *s1 == *s2; ++s1, ++s2)
+        if(*s1 == 0)
+            return 0;
+    return *(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1;
+}
+
+char *my_strcat(char *dest, const char *src)
+{
+    size_t i,j;
+    for (i = 0; dest[i] != '\0'; i++)
+        ;
+    for (j = 0; src[j] != '\0'; j++)
+        dest[i+j] = src[j];
+    dest[i+j] = '\0';
+    return dest;
+}
+
+
 int Pcheck ( char * ist){
     int lcount =0;
     int rcount =0;
@@ -128,8 +148,8 @@ char *progScanner(FILE *ipf, char* instruction){//,char *ist ){
         token = strtok(NULL, ", \t\r\n()");
         /* walk through other tokens */
         while( token != NULL ) {
-            strcat(instruction, " ");
-            strcat(instruction, token);
+            my_strcat(instruction, " ");
+            my_strcat(instruction, token);
            // printf( "----- %s\n", instruction );
 
             token = strtok(NULL,", \t\r\n()" );
@@ -151,114 +171,114 @@ char *regNumberConverter(char * instruction){
     //*instruction = '\0';
     /* walk through other tokens */
     while( token != NULL ) {
-        if((strcmp(token,"$zero")==0)||(strcmp(token,"$0")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$0");
-        }else if((strcmp(token,"$at")==0)||(strcmp(token,"$1")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$1");
-        }else if((strcmp(token,"$v0")==0)||(strcmp(token,"$2")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$2");
-        }else if((strcmp(token,"$v1")==0)||(strcmp(token,"$3")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$3");
-        }else if((strcmp(token,"$a0")==0)||(strcmp(token,"$4")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$4");
-        }else if((strcmp(token,"$a1")==0)||(strcmp(token,"$5")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$5");
-        }else if((strcmp(token,"$a2")==0)||(strcmp(token,"$6")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$6");
-        }else if((strcmp(token,"$a3")==0)||(strcmp(token,"$7")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$7");
-        }else if((strcmp(token,"$t0")==0)||(strcmp(token,"$8")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$8");
-        }else if((strcmp(token,"$t1")==0)||(strcmp(token,"$9")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$9");
-        }else if((strcmp(token,"$t2")==0)||(strcmp(token,"$10")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$10");
-        }else if((strcmp(token,"$t3")==0)||(strcmp(token,"$11")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$11");
-        }else if((strcmp(token,"$t4")==0)||(strcmp(token,"$12")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$12");
-        }else if((strcmp(token,"$t5")==0)||(strcmp(token,"$13")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$13");
-        }else if((strcmp(token,"$t6")==0)||(strcmp(token,"$14")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$14");
-        }else if((strcmp(token,"$t7")==0)||(strcmp(token,"$15")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$15");
-        }else if((strcmp(token,"$s0")==0)||(strcmp(token,"$16")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$16");
-        }else if((strcmp(token,"$s1")==0L)||(strcmp(token,"$17")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$17");
-        }else if((strcmp(token,"$s2")==0)||(strcmp(token,"$18")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$18");
-        }else if((strcmp(token,"$s3")==0)||(strcmp(token,"$19")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$19");
-        }else if((strcmp(token,"$s4")==0)||(strcmp(token,"$20")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$20");
-        }else if((strcmp(token,"$s5")==0)||(strcmp(token,"$21")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$21");
-        }else if((strcmp(token,"$s6")==0)||(strcmp(token,"$22")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$22");
-        }else if((strcmp(token,"$s7")==0)||(strcmp(token,"$23")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$23");
-        }else if((strcmp(token,"$t8")==0)||(strcmp(token,"$24")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$24");
-        }else if((strcmp(token,"$t9")==0)||(strcmp(token,"$25")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$25");
-        }else if((strcmp(token,"$k0")==0)||(strcmp(token,"$26")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$26");
-        }else if((strcmp(token,"$k1")==0)||(strcmp(token,"$27")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$27");
-        }else if((strcmp(token,"$gp")==0)||(strcmp(token,"$28")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$28");
-        }else if((strcmp(token,"$sp")==0)||(strcmp(token,"$29")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$29");
-        }else if((strcmp(token,"$fp")==0)||(strcmp(token,"$30")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$30");
-        }else if((strcmp(token,"$ra")==0)||(strcmp(token,"$31")==0)){
-            strcat(instruction, " ");
-            strcat(instruction, "$31");
+        if((my_strcmp(token,"$zero")==0)||(my_strcmp(token,"$0")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$0");
+        }else if((my_strcmp(token,"$at")==0)||(my_strcmp(token,"$1")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$1");
+        }else if((my_strcmp(token,"$v0")==0)||(my_strcmp(token,"$2")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$2");
+        }else if((my_strcmp(token,"$v1")==0)||(my_strcmp(token,"$3")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$3");
+        }else if((my_strcmp(token,"$a0")==0)||(my_strcmp(token,"$4")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$4");
+        }else if((my_strcmp(token,"$a1")==0)||(my_strcmp(token,"$5")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$5");
+        }else if((my_strcmp(token,"$a2")==0)||(my_strcmp(token,"$6")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$6");
+        }else if((my_strcmp(token,"$a3")==0)||(my_strcmp(token,"$7")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$7");
+        }else if((my_strcmp(token,"$t0")==0)||(my_strcmp(token,"$8")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$8");
+        }else if((my_strcmp(token,"$t1")==0)||(my_strcmp(token,"$9")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$9");
+        }else if((my_strcmp(token,"$t2")==0)||(my_strcmp(token,"$10")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$10");
+        }else if((my_strcmp(token,"$t3")==0)||(my_strcmp(token,"$11")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$11");
+        }else if((my_strcmp(token,"$t4")==0)||(my_strcmp(token,"$12")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$12");
+        }else if((my_strcmp(token,"$t5")==0)||(my_strcmp(token,"$13")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$13");
+        }else if((my_strcmp(token,"$t6")==0)||(my_strcmp(token,"$14")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$14");
+        }else if((my_strcmp(token,"$t7")==0)||(my_strcmp(token,"$15")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$15");
+        }else if((my_strcmp(token,"$s0")==0)||(my_strcmp(token,"$16")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$16");
+        }else if((my_strcmp(token,"$s1")==0L)||(my_strcmp(token,"$17")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$17");
+        }else if((my_strcmp(token,"$s2")==0)||(my_strcmp(token,"$18")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$18");
+        }else if((my_strcmp(token,"$s3")==0)||(my_strcmp(token,"$19")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$19");
+        }else if((my_strcmp(token,"$s4")==0)||(my_strcmp(token,"$20")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$20");
+        }else if((my_strcmp(token,"$s5")==0)||(my_strcmp(token,"$21")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$21");
+        }else if((my_strcmp(token,"$s6")==0)||(my_strcmp(token,"$22")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$22");
+        }else if((my_strcmp(token,"$s7")==0)||(my_strcmp(token,"$23")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$23");
+        }else if((my_strcmp(token,"$t8")==0)||(my_strcmp(token,"$24")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$24");
+        }else if((my_strcmp(token,"$t9")==0)||(my_strcmp(token,"$25")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$25");
+        }else if((my_strcmp(token,"$k0")==0)||(my_strcmp(token,"$26")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$26");
+        }else if((my_strcmp(token,"$k1")==0)||(my_strcmp(token,"$27")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$27");
+        }else if((my_strcmp(token,"$gp")==0)||(my_strcmp(token,"$28")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$28");
+        }else if((my_strcmp(token,"$sp")==0)||(my_strcmp(token,"$29")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$29");
+        }else if((my_strcmp(token,"$fp")==0)||(my_strcmp(token,"$30")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$30");
+        }else if((my_strcmp(token,"$ra")==0)||(my_strcmp(token,"$31")==0)){
+            my_strcat(instruction, " ");
+            my_strcat(instruction, "$31");
         }else if(atoi(token)>31){
             if (strstr(token,"$")!=NULL) {
                 printf("Register out of bounds, %d is higher than 31 \n", atoi(token));
                 exit(1);
             }
             else {
-                strcat(instruction, " ");
-                strcat(instruction,token);
+                my_strcat(instruction, " ");
+                my_strcat(instruction,token);
             }
         }else {
-            strcat(instruction, " ");
-            strcat(instruction,token);
+            my_strcat(instruction, " ");
+            my_strcat(instruction,token);
         }
 
 
@@ -280,35 +300,35 @@ inst parser(char *instruction){
     s2 =strtok (NULL, " ");
     s3 =strtok (NULL, " \r\n");
 // check format
-    if(strcmp(op,"add")==0){
+    if(my_strcmp(op,"add")==0){
         //trcpy(instruction,"add");
         type ="R";
         ninst.op = add;
-    }else if(strcmp(op,"sub")==0){
+    }else if(my_strcmp(op,"sub")==0){
         //strcpy(instruction,"sub");
         type ="R";
         ninst.op = sub;
-    }else if(strcmp(op,"mul")==0){
+    }else if(my_strcmp(op,"mul")==0){
         //strcpy(instruction,"mul");
         type ="R";
         ninst.op = mul;
-    }else if(strcmp(op,"addi")==0){
+    }else if(my_strcmp(op,"addi")==0){
         //strcpy(instruction,"addi");
         type ="I";
         ninst.op = addi;
-    }else if(strcmp(op,"beq")==0){
+    }else if(my_strcmp(op,"beq")==0){
         // strcpy(instruction,"beq");
         type ="I";
         ninst.op = beq;
-    }else if(strcmp(op,"lw")==0){
+    }else if(my_strcmp(op,"lw")==0){
         type ="M";
         // strcpy(instruction,"lw");
         ninst.op = lw;
-    }else if(strcmp(op,"sw")==0){
+    }else if(my_strcmp(op,"sw")==0){
         type ="M";
         //strcpy(instruction,"sw");
         ninst.op = sw;
-    }else if(strcmp(op,"haltSimulation")==0) {
+    }else if(my_strcmp(op,"haltSimulation")==0) {
         type ="S";
         //strcpy(instruction,"haltSimulation");
         ninst.op = haltSimulation;
@@ -694,10 +714,10 @@ int main (int argc, char *argv[]){
     }
     printf("\n");
     if(argc==7){
-        if(strcmp("-s",argv[1])==0){
+        if(my_strcmp("-s",argv[1])==0){
             sim_mode=SINGLE;
         }
-        else if(strcmp("-b",argv[1])==0){
+        else if(my_strcmp("-b",argv[1])==0){
             sim_mode=BATCH;
         }
         else{
